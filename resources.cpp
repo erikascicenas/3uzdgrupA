ce1f2e8a6b141eec14fb3e091305822f9ad8c9d3
erikascicenas <erikas.cicenas@ftmc.lt>
2023-09-29 14:38:53 +0300

    first WORKING draft

--- resources.cpp
+++ resources.cpp
@@ -1,5 +1,30 @@
-#include <resources.h>
+#include "resources.h"
 
-void access_res1(std::string out, int n) {
-    //TODO
+#include <cstdlib>
+
+#include <iostream>
+
+SpinLock lock_res1;
+
+SpinLock lock_res2;
+
+SpinLock lock_res3;
+
+void access_res1(const std::string& out, int n, int id) {
+    std::cout << '\n' << id << ':' << out << std::endl;
+    long fact = 1;
+    for(int i = 1; i <= n; ++i)
+        fact *= i;
+    
+    std::cout << "The factorial is " << fact << std::endl;
+}
+
+void access_res2(int n) {
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < 1000000; ++j)
+            i*i*j*j*i*j;
+}
+
+int access_res3() {
+    return rand();
 }
--- resources.h
+++ resources.h
@@ -1,19 +1,52 @@
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
-#include <mutex>
+#include <atomic>
 #include <string>
+
+class SpinLock {
+    public:
+    void lock() {
+        while(m_lock.test_and_set()) {/*nop*/}
+    }
+
+    void unlock() {
+        m_lock.clear();
+    }
+
+    private:
+    std::atomic_flag m_lock = ATOMIC_FLAG_INIT;
+};
 
 /**
  * Use this to lock function access_res1
 */
-volatile std::mutex lock_res1;
+extern SpinLock lock_res1;
 /**
  * Output string to stdout whilst also computing a factorial to simulate a critical section. Function does not handle acquiring or freeing the lock.
  * \param out string to output first
  * \param n number of which we compute and output the factorial of n.
- * \exception std::runtime_error thrown when lock is not acquired by any thread.
+ * \param id id of thread
 */
-void access_res1(std::string out, int n);
+void access_res1(const std::string& out, int n, int id);
+
+/**
+ * Use this to lock function access_res2
+*/
+extern SpinLock lock_res2;
+/**
+ * sleep
+ * \param n length
+*/
+void access_res2(int n);
+
+/**
+ * Use this to lock function access_res2
+*/
+extern SpinLock lock_res3;
+/**
+ * Generate one random number
+*/
+int access_res3();
 
 #endif //RESOURCES_H
