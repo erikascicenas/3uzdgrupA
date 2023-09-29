fb6c2a5be2d5f90c95c0b43d21c2b599429eab35
erikascicenas <erikas.cicenas@ftmc.lt>
2023-09-29 12:17:56 +0300

    adding prototype of first resource simulation

--- resources.cpp
+++ resources.cpp
@@ -1,1 +1,5 @@
 #include <resources.h>
+
+void access_res1(std::string out, int n) {
+    //TODO
+}
--- resources.h
+++ resources.h
@@ -1,7 +1,19 @@
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
-//TODO
+#include <mutex>
+#include <string>
 
+/**
+ * Use this to lock function access_res1
+*/
+volatile std::mutex lock_res1;
+/**
+ * Output string to stdout whilst also computing a factorial to simulate a critical section. Function does not handle acquiring or freeing the lock.
+ * \param out string to output first
+ * \param n number of which we compute and output the factorial of n.
+ * \exception std::runtime_error thrown when lock is not acquired by any thread.
+*/
+void access_res1(std::string out, int n);
 
 #endif //RESOURCES_H
